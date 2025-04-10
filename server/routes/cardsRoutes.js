1ac338a27a6afe5c1e08ff0e14b5d6d1a3013cdc
K Harsh <harshnaidu2u@gmail.com>
2025-04-10 11:17:31 +0530

    server: quiz-stat route

--- server/routes/cardsRoutes.js
+++ server/routes/cardsRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { postSheetAnalysis, getStatistics, getSheetDataTable, getSheetNames, postSheetNames, getCourseStatistics } = require("../controllers/statisticsController");
+const { postSheetAnalysis, getStatistics, getSheetDataTable, getSheetNames, postSheetNames, getCourseStatistics, getQuizStatistics } = require("../controllers/statisticsController");
 
 router.route("/analyze-sheet").post(postSheetAnalysis).get(getStatistics);
 router.route("/sheet-names").post(postSheetNames).get(getSheetNames);
 router.get("/sheet-rows-data", getSheetDataTable);
 router.get("/course-statistics", getCourseStatistics);
+router.get("/quiz-data", getQuizStatistics);
 
 module.exports = router;
 
